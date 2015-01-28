dde8a2e62caec362b59c34105c1974e38fcfc70f
Krzysztof Matczak <kmatczak@gmail.com>
2015-01-28 17:13:04 +0100

    sysfs_handler extracted; compillable; not tested on HW

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
-obj-m += led1.o
-EXTRA_CFLAGS:=-g
+obj-m += led_driver.o
+led_driver-objs := led1.o sysfs_handler.o
+
+EXTRA_CFLAGS:=-g -Wno-declaration-after-statement -Wno-unused-result
 KERNELDIR:=/home/ngnlab/workspace/buildroot-black/output/build/linux-7f280334068b7c875ade51f8f3921ab311f0c824
 PWD:=$(shell pwd)
 
@@ -8,4 +10,4 @@ all:
 	make -C $(KERNELDIR) ARCH=arm CROSS_COMPILE=/home/ngnlab/workspace/buildroot-black/output/host/usr/bin/arm-linux- SUBDIRS=$(PWD) modules
 
 clean:
-	rm -rf *.o .depend.*.cmd *.ko *.mod.c .tmp_versions .led1*	
+	rm -rf *.o .depend.*.cmd *.ko *.mod.c .tmp_versions .led1*  .sysfs_handler* 	
--- led1.c
+++ led1.c
@@ -4,25 +4,31 @@
 #include <linux/gpio.h>
 #include <linux/delay.h>
 //#include <linux/interrupt.h>
-#include <linux/kobject.h>
+//#include <linux/kobject.h>
 #include <linux/kthread.h>
-
+#include "led1.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int  gpio=54;
-const char* mod_txt[] = {"normal","blink","timeout"};
-typedef enum modes {NORMAL, BLINK, TIMEOUT} Mode; 
 
-static Mode led_mode = NORMAL;
+//extern definitions
+extern int sysfs_init(void);
+extern int sysfs_cleanup(void);
+
+
+//extern declarations
+int blink_interval=50;
+int timeout_interval=5000;
+Mode led_mode = NORMAL;
+int  gpio=54;
+
 
 struct task_struct* blink_task; 
 struct task_struct* timeout_task; 
 
-int blink_interval=50;
-int timeout_interval=5000;
 
-static int led_control(int val){
+
+int led_control(int val){
 
 //    printk(KERN_INFO "led_control called !\n");
     gpio_set_value(gpio, val);
@@ -42,7 +48,6 @@ static void led_blink(int interval){
     }
 
     led_control(0);
-    do_exit(0);
 
 }
 
@@ -52,153 +57,40 @@ static void led_timer(int time){
     msleep(time);
 
     led_control(0);
-    do_exit(0);
 }
 
 
 
-//----sysfs files creation---------------------------------
 
 
-static char mode[20]; 
-static int control;
-
-
-static ssize_t mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    //return sprintf(buf, "%d\n", mode);
+int stop_led_threads(void){
     
-    switch(led_mode){
-        case BLINK:
-            return sprintf(buf,"%s [%s] %s=%d\n", mod_txt[0], mod_txt[1], mod_txt[2], timeout_interval);
-            break;
-        case TIMEOUT:
-            return sprintf(buf,"%s %s [%s=%d]\n", mod_txt[0], mod_txt[1], mod_txt[2], timeout_interval);
-            break;
-        case NORMAL:
-        default:
-            return sprintf(buf,"[%s] %s %s=%d\n", mod_txt[0], mod_txt[1], mod_txt[2], timeout_interval);
-            break;
-    }
-    
+    if (timeout_task != NULL) kthread_stop(timeout_task);
+    if (blink_task != NULL) kthread_stop(blink_task);
+
+    return 0;
 }
 
-static ssize_t mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    
-    strncpy(mode,buf,20);
-    printk(KERN_INFO "fetched mode: %s", mode);
-    
-    if ( NULL != strstr(mode, mod_txt[0]) ) led_mode = NORMAL;
-    else if ( NULL != strstr(mode, mod_txt[1]) ) led_mode = BLINK;
-    else if ( NULL != strstr(mode, mod_txt[2]) ) {
-        led_mode = TIMEOUT;
-        char* pch; 
-        pch = strchr(mode,'=');
-        if (pch != NULL){
-            ++pch;
-            if ( 0 > sscanf(pch, "%d", &timeout_interval) ) {
-                printk(KERN_ERR "timeout conversion error ! \n");
-                return -1;
-            }
-       }
-    }
-    else {
-        printk(KERN_ERR "mode not supported or wrong syntax\n");
-        return -EIO;
-    }
 
-    return count;
-}
+int start_blink_thread(void){
 
-static void stop_led_threads(void){
-if (timeout_task != NULL) kthread_stop(timeout_task);
-if (blink_task != NULL) kthread_stop(blink_task);
+    blink_task = kthread_run(&led_blink, &blink_interval,"blink_thread");
+    printk(KERN_INFO"Kernel Thread : %s\n",blink_task->comm);
+
+    return 0;
 }
 
 
 
-static ssize_t control_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf, "%d\n", control);
+
+int  start_timeout_thread(void){
+
+    timeout_task = kthread_run(&led_timer, &timeout_interval,"timeout_thread");
+    printk(KERN_INFO"Kernel Thread : %s\n",timeout_task->comm);
+
+    return 0;
 }
 
-
-static ssize_t control_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    
-    sscanf(buf, "%du", &control);
-
-    switch(control){
-        case 0:   
-            switch(led_mode){
-                case BLINK:
-                    stop_led_threads();
-                    break;
-                case TIMEOUT:
-                    stop_led_threads();
-                    break;
-                case NORMAL:
-                default:
-                    led_control(0);
-                    break;            
-            }
-            break;
-        case 1: 
-            switch(led_mode){
-                case BLINK:
-                    stop_led_threads();
-                    blink_task = kthread_run(&led_blink,(void *)blink_interval,"blink_thread");
-                    printk(KERN_INFO"Kernel Thread : %s\n",blink_task->comm);
-                    break;
-                case TIMEOUT:
-                    stop_led_threads();
-                    timeout_task = kthread_run(&led_timer,(void *)timeout_interval,"timeout_thread");
-                    printk(KERN_INFO"Kernel Thread : %s\n",timeout_task->comm);
-                    break;
-                case NORMAL:
-                default:
-                    led_control(1);
-                    break;
-            }
-            break;
-        default: 
-            printk(KERN_ERR "control attr valid values are 0 and 1\n");
-            return -EIO;
-    }
-
-
-
-    return count;
-}
-
-
-static struct kobj_attribute mode_attribute = __ATTR(mode, 0666, mode_show, mode_store);
-static struct kobj_attribute control_attribute = __ATTR(control, 0666, control_show, control_store);
-
-
-
-/*
- * Create a group of attributes so that we can create and destory them all
- * at once.
- */
-static struct attribute *attrs[] = {
-    &mode_attribute.attr,
-    &control_attribute.attr,
-    NULL,  /* need to NULL terminate the list of attributes */
- };
-
-
-/*
- * An unnamed attribute group will put all of the attributes directly in
- * the kobject directory.  If we specify a name, a subdirectory will be
- * created for the attributes with the directory being the name of the
- * attribute group.
- */
- static struct attribute_group attr_group = {
-    .attrs = attrs,
- };
-
-static struct kobject *example_kobj;
-
-
-//------------------
 
 
 
@@ -206,26 +98,7 @@ static int hello_init(void){
     
     printk(KERN_INFO "Hello world !\n");
 
-    /*
-    * Create a simple kobject with the name of "led_gpio_GPIO",
-    * located under /sys/kernel/
-    *
-    * As this is a simple directory, no uevent will be sent to
-    * userspace.  That is why this function should not be used for
-    * any type of dynamic kobjects, where the name and number are
-    * not known ahead of time.
-    */
-    char led_dir[64];
-    snprintf(led_dir, 64, "led_gpio_%d", gpio);
-    example_kobj = kobject_create_and_add(led_dir, kernel_kobj);
-    if (!example_kobj) return -ENOMEM;
-
-   /* Create the files associated with this kobject */
-    int retval;
-    retval  = sysfs_create_group(example_kobj, &attr_group);
-    if (retval)  kobject_put(example_kobj); 
-  
-
+    sysfs_init();
 
 	if ( 0!= gpio_request(gpio,"usr")){
 	    printk(KERN_ERR "Somsing went wrong  with gpio %d :( \n", gpio );
@@ -249,7 +122,8 @@ static void hello_exit(void){
     gpio_set_value(gpio,0);
     gpio_free(gpio);
     
-    kobject_put(example_kobj);    
+
+    sysfs_cleanup();
     stop_led_threads();
     
     printk(KERN_INFO "Bye\n");
