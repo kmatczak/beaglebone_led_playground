1dca88c7454fee93ce3fb6f67707d00727fcc8b2
Krzysztof Matczak <kmatczak@gmail.com>
2015-01-30 01:21:06 +0100

    trying to fix test_ioctl compilation and runtime problems; not finished yet

--- test_ioctl.c
+++ test_ioctl.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <fcntl.h>
+#include <linux/fs.h>
+//#include <stdlib.h>
 #include <linux/ioctl.h>
 #include "led_ioctl_cmd.h"
 
 
 int main (int argc , char** argv){
 
-printf("---- TEST START ----\n");
+//printf("---- TEST START ----\n");
 
 int fd=0;
 
 if (fd=open(DEVICE,O_RDWR) < 0) {
-    perror("error while file opening\n");
+//    perror("error while file opening\n");
     exit(1);
 }
 
@@ -21,16 +22,16 @@ char *msg="test_message";
 //if ( retval = ioctl(fd, IOC_LED_ON, 0) == -1){
 if ( retval = ioctl(fd, IOCTL_SET_INT, 0) == -1){
     
-    perror("error while ioctl writing\n");
+  //  perror("error while ioctl writing\n");
 }
 else{
  
-    printf("ioctl write OK\n");
+  //  printf("ioctl write OK\n");
 }
 
 close(fd);
 
-printf("---- TEST STOP ----\n");
+//printf("---- TEST STOP ----\n");
 
 
 return 0;
